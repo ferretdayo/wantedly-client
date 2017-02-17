50e30a4ff26d41ea465c3b2001178a301e935de7
ferretdayo <ferretdayo@hotmail.co.jp>
2017-02-17 23:57:18 +0900

    router動いた

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
+
+import { ButtonsModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { LoginComponent } from './login/login.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ButtonsModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: '',
+        pathMatch: 'full',
+        component: LoginComponent,
+      },
+      {
+        path: 'create',
+        pathMatch: 'full',
+        component: CreateUserComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
