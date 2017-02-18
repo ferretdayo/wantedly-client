782424200eac32830459a9cca14c0daedac05eb4
ferretdayo <ferretdayo@hotmail.co.jp>
2017-02-18 11:22:51 +0900

    [WIP]create login structure

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../user.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
+  msg: string
+  email: string
+  password: string
 
-  constructor() { }
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    this.userService.loginUser({
+        email: this.email, 
+        password: this.password
+      })
+      .subscribe(
+        data => {
+          this.email = ""
+          this. password = ""
+          this.msg = "success to create user"
+        },
+        error =>  this.msg = <any>error
+      );
   }
 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -26,12 +26,25 @@ export class UserService {
     return this.jsonp.post("http://127.0.0.1:3000/users", headers, options)
       .map(this.extractData)
    */
-
+  /**
+   * ユーザの作成のリクエストの送信
+   */
   createUser(data: Object): Observable<any[]>{
     let body = JSON.stringify(data);
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
     return this.http.post("http://127.0.0.1:3000/users", body, options)
+      .map(this.extractData)
+  }
+
+  /**
+   * ログインのリクエストの送信
+   */
+  loginUser(data: Object): Observable<any[]>{
+    let body = JSON.stringify(data);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post("http://127.0.0.1:3000/logins", body, options)
       .map(this.extractData)
   }
 
