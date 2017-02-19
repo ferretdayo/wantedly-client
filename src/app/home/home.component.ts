3014288543fd3d2521507fcec16250c7b804aee0
ferretdayo <ferretdayo@hotmail.co.jp>
2017-02-19 19:47:43 +0900

    ユーザのタグ情報を取得できた

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 import { UserService } from '../user.service';
+import { SkillService } from '../skill.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [UserService]
+  providers: [UserService, SkillService]
 })
 export class HomeComponent implements OnInit {
 
-  msg: string;
-  users: any[] = [];
-  loginUser: any = {};
+  msg: string
+  users: any[] = []
+  loginUser: any = {}
+  userTag: any[]
+  skill: string
 
   constructor(
-    private userService: UserService
-  ) { }
-
-  ngOnInit() {
+    private userService: UserService,
+    private skillService: SkillService,
+  ) { 
     /**
      * 全ユーザの表示
      */
@@ -28,9 +30,52 @@ export class HomeComponent implements OnInit {
           this.users = data.users
           this.loginUser = data.users[0]
           this.msg = data.user_id
+          this.showSkill();
         },
         error =>  this.msg = <any>error
       );
+  }
+
+  ngOnInit() {
+  }
+
+  /**
+   * タグの追加
+   */
+  addTag(){
+    // 入力がない場合
+    if(this.skill === "")
+      return;
+    console.log(this.skill)
+    // スキルのタグの追加
+    this.skillService.addSkillTag({
+        id: this.loginUser.id,          /* 誰に向けてのタグか */
+        tag: this.skill,                /* タグの値 */
+        add_userid: this.loginUser.id   /* 誰が追加したか */
+      })
+      .subscribe(
+        data => {
+          if(data.status === true){
+            console.log("success to add skillTag");
+          }
+        },
+        error =>  this.msg = <any>error
+      )
+  }
+
+  /**
+   * ログインしたユーザのタグ情報を取得
+   */
+  showSkill(){
+    this.skillService.getUserSkill(this.loginUser.id)
+      .subscribe(
+        data => {
+          if(data.status === true){
+            this.userTag = data.tag
+          }
+        },
+        error =>  this.msg = <any>error
+      )
   }
 
   detail(user_id: string){
